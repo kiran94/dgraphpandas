52f19448661b2da2025b41b9ca290de0706ebc84
kiran94 <kiran_patel94@hotmail.com>
2021-03-27 11:14:39 +0000

    added option to output files

--- dgraphpandas/__main__.py
+++ dgraphpandas/__main__.py
@@ -1,5 +1,7 @@
+from ast import parse
 import logging
 import argparse
+import os
 
 import pandas as pd
 
@@ -26,17 +28,24 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--subject', nargs='+', required=True)
     parser.add_argument('-e', '--edges', nargs='+', required=False)
     parser.add_argument('-t', '--type', required=True)
+    parser.add_argument('-o', '--output_dir', default='.')
 
     args = parser.parse_args()
 
     frame = pd.read_csv(args.file, compression='infer')
     print(frame)
 
-    intrinsic, edges = horizontal_transform(
-        frame, args.subject, args.edges, args.type)
-    print(intrinsic)
-    print(edges)
+    intrinsic, edges = horizontal_transform(frame, args.subject, args.edges, args.type)
+    # print(intrinsic)
+    # print(edges)
 
-    # intrinsic.to_csv('output/intrinsic.csv', index=False)
-    # edges.to_csv('output/edges.csv', index=False)
+    os.makedirs(args.output_dir, exist_ok=True)
+    intrinsic_path = os.path.join(args.output_dir, args.type + '_intrinsic.csv')
+    edges_path = os.path.join(args.output_dir, args.type + '_edges.csv')
+
+    logger.info(f'Writing to {intrinsic_path}')
+    intrinsic.to_csv(intrinsic_path, index=False)
+
+    logger.info(f'Writing to {edges_path}')
+    edges.to_csv(edges_path, index=False)
     # frame.to_csv('data/pokemon_horizontal.zip', compression={'method': 'zip', 'archive_name': 'pokemon_horizontal.csv'}, index=False)
