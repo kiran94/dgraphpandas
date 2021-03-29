b61f6da45f1dfa5d81651954f9eeb1a8e13e3b18
kiran94 <kiran_patel94@hotmail.com>
2021-03-29 13:58:03 +0100

    updated type definitions

--- dgraphpandas/types.py
+++ dgraphpandas/types.py
@@ -11,16 +11,17 @@ default_rdf_type = os.environ.get('DEFAULT_RDF_TYPE', '<xs:string>')
 
 numpy_to_rdf_types = {
     np.dtype('O'): '<xs:string>',
-    np.dtype('int32'): '<xs:integer>',
-    np.dtype('int64'): '<xs:integer>',
-    np.dtype('Int32'): '<xs:integer>',
-    np.dtype('Int64'): '<xs:integer>',
+    np.dtype('int32'): '<xs:int>',
+    np.dtype('int64'): '<xs:int>',
+    np.dtype('Int32'): '<xs:int>',
+    np.dtype('Int64'): '<xs:int>',
     np.dtype('float32'): '<xs:float>',
     np.dtype('float64'): '<xs:float>',
     np.dtype('Float32'): '<xs:float>',
     np.dtype('Float64'): '<xs:float>',
-    np.dtype('datetime64'): '<xs:datetime>',
-    np.dtype('bool'): '<xs:boolean>'
+    np.dtype('datetime64'): '<xs:dateTime>',
+    np.dtype('<M8[ns]'): '<xs:dateTime>',
+    np.dtype('bool'): '<xs:bool>'
 }
 
 
@@ -31,8 +32,7 @@ def find_rdf_types(
     For the columns in the given DataFrame, build a dictionary of column -> rdf type.
     '''
     derived_types = frame.dtypes.to_dict()
-    derived_types = {key: numpy_to_rdf_types.get(
-        value, default_rdf_type) for key, value in derived_types.items()}
+    derived_types = {key: numpy_to_rdf_types.get(value, default_rdf_type) for key, value in derived_types.items()}
 
     if overrides is not None:
         logger.info(f'Overriding rdf types with {overrides}')
