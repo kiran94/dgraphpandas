5a75ef616773b2f1a20179a798aceadc7a4f0d82
kiran94 <kiran_patel94@hotmail.com>
2021-03-29 14:00:01 +0100

    updated netflix example

--- samples/netflix/input/download_data.sh
+++ samples/netflix/input/download_data.sh
@@ -6,4 +6,3 @@ kaggle datasets download shivamb/netflix-shows
 unzip netflix-shows.zip
 rm netflix-shows.zip
 
-cat netflix_titles.csv | awk 'NR!=1' | awk -F, '{ print $2","$2 }' | sort | uniq | awk 'BEGIN {print "id,desc"}; {print $0}' | awk 'NR!=2' > show_types.csv
