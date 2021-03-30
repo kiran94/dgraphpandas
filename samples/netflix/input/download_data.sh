fc3e514fd8167a5952df48a9203d72428a83a291
kiran94 <kiran_patel94@hotmail.com>
2021-03-30 12:02:30 +0100

    added netflix example queries

--- samples/netflix/input/download_data.sh
+++ samples/netflix/input/download_data.sh
@@ -1,6 +1,7 @@
-!#/bin/bash
+#!/bin/bash
 
 # Requires Kaggle - https://www.kaggle.com/docs/api
+echo Downloading DataSet
 kaggle datasets download shivamb/netflix-shows
 
 unzip netflix-shows.zip
--- samples/netflix/publish_upserts.sh
+++ samples/netflix/publish_upserts.sh
@@ -14,3 +14,11 @@ for f in $OUTPUT_DIR/*.gz; do
     echo $f
     dgraph live --tmp $DGRAPH_TEMP_BUFFERS --files $f --upsertPredicate xid --xidmap $XIDMAP_LOCATION --format rdf --batch 500
 done
+
+
+echo Applying Reverse Edges
+curl -sX POST localhost:8080/alter -d 'cast: [uid] @reverse .' | jq .
+curl -sX POST localhost:8080/alter -d 'genre: [uid] @reverse .' | jq .
+curl -sX POST localhost:8080/alter -d 'director: [uid] @reverse .' | jq .
+curl -sX POST localhost:8080/alter -d 'rating: [uid] @reverse .' | jq .
+curl -sX POST localhost:8080/alter -d 'description: string @index(term) .' | jq .
