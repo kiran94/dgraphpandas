28e3093f6b9475f78cd7c0999e730d1947c309b0
kiran94 <kiran_patel94@hotmail.com>
2021-03-31 15:15:48 +0100

    added tests for types

--- dgraphpandas/types.py
+++ dgraphpandas/types.py
@@ -3,7 +3,6 @@ import os
 from typing import Dict
 import logging
 import numpy as np
-import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -27,27 +26,14 @@ numpy_str_to_rdf_types = {
 }
 
 numpy_to_rdf_types = {
-    key: np.dtype(key) for key, value in numpy_str_to_rdf_types.items()
+    key: np.dtype(key) for key, _ in numpy_str_to_rdf_types.items()
 }
 
 
-def find_rdf_types(
-        frame: pd.DataFrame,
-        overrides: Dict[str, str] = None) -> Dict[str, str]:
-    '''
-    For the columns in the given DataFrame, build a dictionary of column -> rdf type.
-    '''
-    derived_types = frame.dtypes.to_dict()
-    derived_types = {key: numpy_to_rdf_types.get(value, default_rdf_type) for key, value in derived_types.items()}
+def find_rdf_types(types: Dict[str, str]):
 
-    if overrides is not None:
-        logger.info(f'Overriding rdf types with {overrides}')
-        derived_types.update(overrides)
-
-    return derived_types
-
-
-def find_rdf_types_2(frame: pd.DataFrame, types: Dict[str, str]):
+    if types is None:
+        raise ValueError('types')
 
     resolved_types = {}
     for col, type in types.items():
---
+++ tests/test_types.py
@@ -0,0 +1,90 @@
+import unittest
+
+from dgraphpandas.types import find_rdf_types, default_rdf_type
+
+
+class TypesTests(unittest.TestCase):
+
+    def test_find_rdf_types_null_types_exception(self):
+        '''
+        Ensures when the types passed are null,
+        then a value error is raised.
+        '''
+        with self.assertRaises(ValueError):
+            find_rdf_types(None)
+
+    def test_find_rdf_types_some_types_resolvable(self):
+        '''
+        Ensures when some of the types are resolvable
+        to rdf those are resolved but the unresolvable
+        are given the default rdf type.
+        '''
+        types = {
+            'column1': 'int32',
+            'column2': 'float32',
+            'column3': 'something_unknown'
+        }
+
+        expected = {
+            'column1': '<xs:int>',
+            'column2': '<xs:float>',
+            'column3': default_rdf_type
+        }
+
+        resolved_types = find_rdf_types(types)
+        self.assertEqual(expected, resolved_types)
+
+    def test_find_rdf_types_all_resolvable(self):
+        '''
+        Ensures when all the types are resolvable,
+        they are all resolved.
+        '''
+        types = {
+                'column1': 'object',
+                'column2': 'O',
+                'column3': 'int32',
+                'column4': 'int64',
+                'column5': 'Int32',
+                'column6': 'Int64',
+                'column7': 'float32',
+                'column8': 'float64',
+                'column9': 'Float32',
+                'column10': 'Float64',
+                'column11': 'datetime64',
+                'column12': '<M8[ns]',
+                'column13': 'bool'
+        }
+
+        expected = {
+                'column1': '<xs:string>',
+                'column2': '<xs:string>',
+                'column3': '<xs:int>',
+                'column4': '<xs:int>',
+                'column5': '<xs:int>',
+                'column6': '<xs:int>',
+                'column7': '<xs:float>',
+                'column8': '<xs:float>',
+                'column9': '<xs:float>',
+                'column10': '<xs:float>',
+                'column11': '<xs:dateTime>',
+                'column12': '<xs:dateTime>',
+                'column13': '<xs:bool>'
+        }
+
+        resolved_types = find_rdf_types(types)
+        self.assertEqual(expected, resolved_types)
+
+    def test_find_rdf_types_none_resolvable(self):
+        '''
+        Ensures when none of the columns can be resolved,
+        they are all resolved to default rdf type
+        '''
+        types = {
+                'column1': 'unknown',
+                'column2': 'unknown2',
+                'column3': 'unknown3',
+                'column4': 'unknown4'
+        }
+
+        resolved_types = find_rdf_types(types)
+        self.assertEqual(list(resolved_types.values()), [default_rdf_type]*4)
