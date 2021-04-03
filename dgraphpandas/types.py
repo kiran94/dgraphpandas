60d341c498d2b1f0549358c90eb8c4cfcce011b2
Kiran Patel <7103956+kiran94@users.noreply.github.com>
2021-04-03 19:08:00 +0100

    build: added github action (#2)
    * Create python-package.yml
    
    * removed capital numpy types
    
    * cleaned up gh workfow
    
    * cleaned up gh workfow

--- dgraphpandas/types.py
+++ dgraphpandas/types.py
@@ -14,12 +14,8 @@ numpy_str_to_rdf_types = {
     'O': '<xs:string>',
     'int32': '<xs:int>',
     'int64': '<xs:int>',
-    'Int32': '<xs:int>',
-    'Int64': '<xs:int>',
     'float32': '<xs:float>',
     'float64': '<xs:float>',
-    'Float32': '<xs:float>',
-    'Float64': '<xs:float>',
     'datetime64': '<xs:dateTime>',
     '<M8[ns]': '<xs:dateTime>',
     'bool': '<xs:bool>'
