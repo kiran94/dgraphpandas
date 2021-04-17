ad4516c5f2fce3e308449f8e483175e2bc48719c
kiran94 <kiran_patel94@hotmail.com>
2021-04-17 14:37:25 +0100

    implemented pre schema generation

--- dgraphpandas/__main__.py
+++ dgraphpandas/__main__.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 
 from dgraphpandas import __version__, __description__, to_rdf
+from dgraphpandas.strategies.schema import create_schema
 
 pd.set_option('mode.chained_assignment', None)
 
@@ -12,16 +13,15 @@ pd.set_option('mode.chained_assignment', None)
 def main():
     parser = argparse.ArgumentParser(description=__description__)
     parser.add_argument('-x', '--method', choices=['upserts', 'schema'], default='upserts')
-    parser.add_argument('-f', '--file', required=True, help='The Data File (CSV) to convert into RDF.')
+    parser.add_argument('-f', '--file', required=False, help='The Data File (CSV) to convert into RDF.')
     parser.add_argument('-c', '--config', required=True, help='The DgraphPandas Configuration. See Documentation for options/examples.')
-    parser.add_argument('-ck', '--config_file_key', required=True, help='The Entry in the Configuration to use for this passed file.')
+    parser.add_argument('-ck', '--config_file_key', required=False, help='The Entry in the Configuration to use for this passed file.')
     parser.add_argument('-o', '--output_dir', default='.', help='The output directory to write files.')
     parser.add_argument('--console', action='store_true', default=False, help='Write the Preprocessed DataFrames to console (for debugging)')
     parser.add_argument('--export_csv', action='store_true', default=False, help='Write the Preprocessed DataFrame to CSV (for debugging)')
     parser.add_argument('--encoding', default=os.environ.get('DGRAPHPANDAS_ENCODING', 'utf-8'), help='The Encoding to write files.')
     parser.add_argument('--chunk_size', default=10_000_000, type=int, help='Process and output in chunks rather all at once')
     parser.add_argument('--gz_compression_level', default=9, type=int, help='Compression level to set output gzip files to')
-
     parser.add_argument('--key_separator')
     parser.add_argument('--add_dgraph_type_records', default=True)
     parser.add_argument('--drop_na_intrinsic_objects', default=True)
@@ -44,23 +44,33 @@ def main():
     except ImportError as e:
         logger.debug(e)
 
-    if args.method == 'upserts':
-        options = {
-            'key_separator': args.key_separator,
-            'add_dgraph_type_records': args.add_dgraph_type_records,
-            'drop_na_intrinsic_objects': args.drop_na_intrinsic_objects,
-            'drop_na_edge_objects': args.drop_na_edge_objects,
-            'illegal_characters': args.illegal_characters,
-            'illegal_characters_intrinsic_object': args.illegal_characters_intrinsic_object,
-            'console': args.console,
-            'export_csv': args.export_csv,
-            'chunk_size': args.chunk_size
-        }
-        options = {key: value for key, value in options.items() if value is not None and value is not False}
+    logger.debug(args)
 
+    options = {
+        'key_separator': args.key_separator,
+        'add_dgraph_type_records': args.add_dgraph_type_records,
+        'drop_na_intrinsic_objects': args.drop_na_intrinsic_objects,
+        'drop_na_edge_objects': args.drop_na_edge_objects,
+        'illegal_characters': args.illegal_characters,
+        'illegal_characters_intrinsic_object': args.illegal_characters_intrinsic_object,
+        'console': args.console,
+        'export_csv': args.export_csv,
+        'chunk_size': args.chunk_size
+    }
+    options = {key: value for key, value in options.items() if value is not None and value is not False}
+
+    if args.method == 'upserts':
+        if args.file is None:
+            raise ValueError('file must be provided in upsert mode')
+        elif args.config_file_key is None:
+            raise ValueError('config_file_key must be provided in upsert mode')
         to_rdf(args.file, args.config, args.config_file_key, args.output_dir, export_rdf=True, **(options))
 
-        to_rdf(args.file, global_config, args.config_file_key, args.output_dir, export_rdf=True, **(options))
+    elif args.method == 'schema':
+        create_schema(args.config, **(options))
+
+    else:
+        raise NotImplementedError(args.method)
 
 
 if __name__ == '__main__':
---
+++ tests/strategies/test_schema.py
@@ -0,0 +1,381 @@
+from unittest.mock import patch, Mock, mock_open
+
+import pytest
+import pandas as pd
+from pandas.testing import assert_frame_equal
+from parameterized import parameterized
+
+from dgraphpandas.strategies.schema import create_schema
+
+
+def test_create_schema_none_source_config():
+    '''
+    Ensures when the source config is None, then an error is raised
+    '''
+    with pytest.raises(ValueError):
+        create_schema(None)
+
+
+@patch('dgraphpandas.strategies.schema.json.load')
+@patch('builtins.open', callable=mock_open)
+def test_create_schema_no_files(mock_file: Mock, mock_json_loads: Mock):
+    '''
+    Ensures when no files is defined in config then an error is raised
+    '''
+    mock_json_loads.return_value = {"some_other_entry": {}}
+    with pytest.raises(KeyError, match='files'):
+        create_schema('my_config.json')
+
+    mock_file.assert_called()
+    mock_json_loads.assert_called()
+
+    args, kwargs = mock_file.call_args_list[0]
+    assert args == ('my_config.json', 'r')
+    assert kwargs == {}
+
+
+@patch('dgraphpandas.strategies.schema.json.load')
+@patch('builtins.open', callable=mock_open)
+def test_create_schema_file_path_config(mock_file: Mock, mock_json_loads: Mock):
+    '''
+    Ensures when a file path is passed which has files attribute, is it loaded
+    but nothing is returned
+    '''
+    mock_json_loads.return_value = {"files": {}}
+    result = create_schema('my_config.json')
+
+    assert result is None
+
+    mock_file.assert_called()
+    mock_json_loads.assert_called()
+
+    args, kwargs = mock_file.call_args_list[0]
+    assert args == ('my_config.json', 'r')
+    assert kwargs == {}
+
+
+@parameterized.expand([
+    ###
+    (
+        'single_file',
+        {
+            'files': {
+                "animal": {
+                    'edge_fields': ['habitat']
+                }
+            }
+        },
+        'animal does not have subject_fields.'
+    ),
+    ###
+    (
+        'multiple_files_after_first_has_no_subject_fields',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'edge_fields': ['habitat']
+                },
+                "habitat": {
+                    'edge_fields': ['animal_id']
+                }
+            }
+        },
+        'habitat does not have subject_fields.'
+    ),
+    ###
+    (
+        'multiple_files_first_has_no_subject_fields',
+        {
+            'files': {
+                "habitat": {
+                    'edge_fields': ['animal_id']
+                },
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'edge_fields': ['habitat']
+                }
+            }
+        },
+        'habitat does not have subject_fields.'
+    ),
+])
+def test_create_schema_missing_subject_fields(name, config, expected_error_message):
+    '''
+    Ensures when the schema is missing subject fields, then an error is raised
+    '''
+    with pytest.raises(ValueError, match=expected_error_message):
+        create_schema(config)
+
+
+@parameterized.expand([
+    ###
+    (
+        'only_subject_fields',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                }
+            }
+        },
+        pd.DataFrame(data={
+            'column': ['animal'],
+            'type': ['string'],
+            'table': 'animal',
+            'options': None
+        })
+    ),
+    ###
+    (
+        'with_type_overrides',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    }
+                }
+            }
+        },
+        pd.DataFrame(data={
+            'column': ['animal', 'legs', 'weight'],
+            'type': ['string', 'int', 'float'],
+            'table': ['animal']*3,
+            'options': [None]*3
+        })
+    ),
+    ###
+    (
+        'with_edge_fields',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    },
+                    'edge_fields': ['habitat_id']
+                }
+            }
+        },
+        pd.DataFrame(data={
+            'column': ['animal', 'legs', 'weight', 'habitat'],
+            'type': ['string', 'int', 'float', 'uid'],
+            'table': ['animal']*4,
+            'options': [None]*4
+        })
+    ),
+    ###
+    (
+        'with_csv_edges',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    },
+                    'edge_fields': ['habitat_id'],
+                    'csv_edges': ['group_id']
+                }
+            }
+        },
+        pd.DataFrame(data=[
+            ('animal', 'string', 'animal', None),
+            ('legs', 'int', 'animal', None),
+            ('weight', 'float', 'animal', None),
+            ('habitat', 'uid', 'animal', None),
+            ('group', 'uid', 'animal', None),
+        ], columns=['column', 'type', 'table', 'options'])
+    ),
+    ###
+    (
+        'with_ignore_fields',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    },
+                    'edge_fields': ['habitat_id', 'group_id'],
+                    'ignore_fields': ['group_id']
+                }
+            }
+        },
+        pd.DataFrame(data=[
+            ('animal', 'string', 'animal', None),
+            ('legs', 'int', 'animal', None),
+            ('weight', 'float', 'animal', None),
+            ('habitat', 'uid', 'animal', None),
+        ], columns=['column', 'type', 'table', 'options'])
+    ),
+    ###
+    (
+        'with_override_edge_fields',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    },
+                    'edge_fields': ['habitat_id'],
+                    'override_edge_name': {
+                        'habitat_id': {
+                            'predicate': 'habo'
+                        }
+                    }
+                }
+            }
+        },
+        pd.DataFrame(data=[
+            ('animal', 'string', 'animal', None),
+            ('legs', 'int', 'animal', None),
+            ('weight', 'float', 'animal', None),
+            ('habo', 'uid', 'animal', None),
+            ('habitat', 'uid', 'animal', None),
+        ], columns=['column', 'type', 'table', 'options'])
+    ),
+    ###
+    (
+        'without_strip_id_from_edge_names',
+        {
+            'strip_id_from_edge_names': False,
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    },
+                    'edge_fields': ['habitat_id']
+                }
+            }
+        },
+        pd.DataFrame(data=[
+            ('animal_id', 'string', 'animal', None),
+            ('legs', 'int', 'animal', None),
+            ('weight', 'float', 'animal', None),
+            ('habitat_id', 'uid', 'animal', None),
+        ], columns=['column', 'type', 'table', 'options'])
+    ),
+    ###
+    (
+        'with_prerename',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    },
+                    'edge_fields': ['habitat_id'],
+                    'pre_rename': {
+                        'legs': 'number_of_legs',
+                        'habitat': 'habo'
+                    }
+                }
+            }
+        },
+        pd.DataFrame(data=[
+            ('animal', 'string', 'animal', None),
+            ('number_of_legs', 'int', 'animal', None),
+            ('weight', 'float', 'animal', None),
+            ('habo', 'uid', 'animal', None),
+        ], columns=['column', 'type', 'table', 'options'])
+    ),
+    ###
+    (
+        'with_options',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    },
+                    'edge_fields': ['habitat_id'],
+                    'options': {
+                        'legs': ['@count', '@index(exact)'],
+                        'habitat': ['@reverse']
+                    }
+                }
+            }
+        },
+        pd.DataFrame(data=[
+            ('animal', 'string', 'animal', None),
+            ('legs', 'int', 'animal', '@count @index(exact)'),
+            ('weight', 'float', 'animal', None),
+            ('habitat', 'uid', 'animal', '@reverse'),
+        ], columns=['column', 'type', 'table', 'options'])
+    ),
+    ###
+    (
+        'with_multiple_files',
+        {
+            'files': {
+                "animal": {
+                    'subject_fields': ['animal_id'],
+                    'type_overrides': {
+                        'legs': 'int',
+                        'weight': 'float'
+                    },
+                    'edge_fields': ['habitat_id'],
+                    'options': {
+                        'legs': ['@count', '@index(exact)'],
+                        'habitat': ['@reverse']
+                    }
+                },
+                'habitat': {
+                    'subject_fields': ['habitat_id'],
+                    'type_overrides': {
+                        'color': 'string',
+                        'sea_level': 'int'
+                    },
+                    'edge_fields': ['country_id'],
+                    'options': {
+                        'color': ['@index(hash)']
+                    }
+                }
+            }
+        },
+        pd.DataFrame(data=[
+            ('animal', 'string', 'animal', None),
+            ('legs', 'int', 'animal', '@count @index(exact)'),
+            ('weight', 'float', 'animal', None),
+            ('habitat', 'uid', 'animal', '@reverse'),
+            ('habitat', 'string', 'habitat', None),
+            ('color', 'string', 'habitat', '@index(hash)'),
+            ('country', 'uid', 'habitat', None),
+            ('sea_level', 'int', 'habitat', None),
+        ], columns=['column', 'type', 'table', 'options'])
+    ),
+
+
+])
+def test_create_schema(name, config, expected_frame):
+    '''
+    Ensures given the parameterized config, the output frame matches the set expected_frame
+    '''
+    frame = create_schema(config)
+
+    frame = frame.sort_values(by=['table', 'column']).reset_index(drop=True)
+    expected_frame = expected_frame.sort_values(by=['table', 'column']).reset_index(drop=True)
+
+    try:
+        assert_frame_equal(frame, expected_frame)
+    except AssertionError:
+        print('Actual Frame \n', frame)
+        print('####')
+        print('Expected Frame \n', expected_frame)
+        raise
